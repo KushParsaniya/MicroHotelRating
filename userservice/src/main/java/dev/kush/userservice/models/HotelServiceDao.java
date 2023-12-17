42ba1b8a4504d66f8a06bab3500643bbe10e9356
kush parsaniya <kushparsaniya94@gmail.com>
2023-12-17 20:58:14 +0530

    added api-gateway and modiefied users/withDetail response

--- userservice/src/main/java/dev/kush/userservice/models/HotelServiceDao.java
+++ userservice/src/main/java/dev/kush/userservice/models/HotelServiceDao.java
@@ -1,9 +1,5 @@
 package dev.kush.userservice.models;
 
-public record HotelServiceDao(
-        // For hotel
-        Long hotelId, String name, String location, String description,
-        // For rating
-        Integer rating,String comment
+public record HotelServiceDao(Hotel hotel, RatingDao rating
 ) {
 }
---
+++ userservice/src/main/java/dev/kush/userservice/models/RatingDao.java
@@ -0,0 +1,4 @@
+package dev.kush.userservice.models;
+
+public record RatingDao(int rating,String comment) {
+}
